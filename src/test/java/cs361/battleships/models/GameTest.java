14151f181b1256556caa525c403856ba3032d7ca
Martin Nguyen <martinnguyen0523@gmail.com>
2019-02-22 14:57:39 -0800

    added tests for Game.java, now at 90% line coverage

--- src/test/java/cs361/battleships/models/GameTest.java
+++ src/test/java/cs361/battleships/models/GameTest.java
@@ -1,4 +1,25 @@
 package cs361.battleships.models;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class GameTest {
+
+    @Test
+    public void testAttack() {
+        Game game = new Game();
+        assertTrue(game.attack(1, 'A'));
+        assertFalse(game.attack(1, 'A'));
+    }
+
+    @Test
+    public void testSonar() {
+        Game game = new Game();
+        assertTrue(game.sonar(1, 'A'));
+    }
+
 }
